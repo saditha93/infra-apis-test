76c732a8084fe5e89c12688211a51fd3c73a9ce4
Saditha <tmsaditha@gmail.com>
2025-10-16 11:16:37 +0530

    update code

--- services/api1/app.py
+++ services/api1/app.py
@@ -1,16 +1,20 @@
-from flask import Flask, jsonify
 import os
 
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+
 
 @app.get("/health")
 def health():
     return {"status": "ok", "service": "api1"}
 
+
 @app.get("/hello")
 def hello():
     name = os.getenv("SERVICE_NAME", "api1")
     return jsonify(message=f"Hello from {name}!")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8001)
